2368e03c595c352e313c91f88be1457f7ab251ee
Jonas <jonasgranbom@hotmail.com>
2023-05-23 20:32:25 +0200

    test

--- main.go
+++ main.go
@@ -5,10 +5,8 @@ import (
 	"net/http"
 	"planetor-reborn/data"
 	"strconv"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/itsjamie/gin-cors"
 
 	"gorm.io/gorm"
 )
@@ -83,22 +81,36 @@ func deleteCelestialBody(c *gin.Context) {
 func enableCors(c *gin.Context) {
 	(*c).Header("Access-Control-Allow-Origin", "*")
 }
+func CORS(c *gin.Context) {
 
+	// First, we add the headers with need to enable CORS
+	// Make sure to adjust these headers to your needs
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Headers", "*")
+	c.Header("Content-Type", "application/json")
+
+	// Second, we handle the OPTIONS problem
+	if c.Request.Method != "OPTIONS" {
+		
+		c.Next()
+
+	} else {
+        
+		// Everytime we receive an OPTIONS request, 
+		// we just return an HTTP 200 Status Code
+		// Like this, Angular can now do the real 
+		// request using any other method than OPTIONS
+		c.AbortWithStatus(http.StatusOK)
+	}
+}
 
 func main() {
   	
 	data.ConnectDatabase()
 
-	router := gin.New()
-	router.Use(cors.Middleware(cors.Config{
-		Origins:        "*",
-		Methods:        "GET, PUT, POST, DELETE",
-		RequestHeaders: "Origin, Authorization, Content-Type",
-		ExposedHeaders: "",
-		MaxAge: 50 * time.Second,
-		Credentials: false,
-		ValidateHeaders: false,
-	}))
+	router := gin.Default()
+	router.Use(CORS)
 
 	router.GET("/api/seed", seed)
 	
