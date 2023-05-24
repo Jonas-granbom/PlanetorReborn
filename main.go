de61b9e70144a0b1f85e5bbe310e33586a7f745f
Jonas <jonasgranbom@hotmail.com>
2023-05-24 10:00:07 +0200

    .

--- main.go
+++ main.go
@@ -79,27 +79,19 @@ func deleteCelestialBody(c *gin.Context) {
 
 }
 
-func CORS(c *gin.Context) {
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
-	// First, we add the headers with need to enable CORS
-	// Make sure to adjust these headers to your needs
-	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Access-Control-Allow-Methods", "*")
-	c.Header("Access-Control-Allow-Headers", "*")
-	c.Header("Content-Type", "application/json")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 
-	// Second, we handle the OPTIONS problem
-	if c.Request.Method != "OPTIONS" {
-		
 		c.Next()
-
-	} else {
-        
-		// Everytime we receive an OPTIONS request, 
-		// we just return an HTTP 200 Status Code
-		// Like this, Angular can now do the real 
-		// request using any other method than OPTIONS
-		c.AbortWithStatus(http.StatusOK)
 	}
 }
 
@@ -108,7 +100,7 @@ func main() {
 	data.ConnectDatabase()
 
 	router := gin.Default()
-	router.Use(CORS)
+	router.Use(CORS())
 
 	router.GET("/api/seed", seed)
 	
